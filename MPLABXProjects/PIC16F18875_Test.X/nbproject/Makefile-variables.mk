634c3feb8a95d20ffaf83da910ea5654136e1eb5
Muthanna A. Attyah <mkhuthir@gmail.com>
2017-03-11 07:31:42 +0400

    first commit

---
+++ MPLABXProjects/PIC16F18875_Test.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PIC16F18875_Test.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PIC16F18875_Test.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pic16f18875test.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pic16f18875test.x.tar
