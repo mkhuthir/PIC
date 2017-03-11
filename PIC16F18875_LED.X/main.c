7035a1100877961092bdd9fa2a7c5dfdb90f37c8
Muthanna A. Attyah <mkhuthir@gmail.com>
2017-03-11 08:35:08 +0400

    Added comments

--- PIC16F18875_LED.X/main.c
+++ PIC16F18875_LED.X/main.c
@@ -1,4 +1,7 @@
 
+// (C) Muthanna A. Attyah Mar 2017
+// Using PIC16F18875 on Curiosity HPC DevBoard
+
 #include "mcc_generated_files/mcc.h"
 
 void main(void)
@@ -7,7 +10,7 @@ void main(void)
     SYSTEM_Initialize();
 
     IO_RA4_SetHigh();
-    IO_RA5_SetLow();
+    IO_RA5_SetHigh();
     IO_RA6_SetHigh();
     IO_RA7_SetHigh();
 }
