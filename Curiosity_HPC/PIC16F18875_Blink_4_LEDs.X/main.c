8c22a732ddd2ded0092cb8aab2fe4cad4d08adb2
mkhuthir <mkhuthir@gmail.com>
2017-05-25 09:49:48 +0400

    rename

---
+++ Curiosity_HPC/PIC16F18875_Blink_4_LEDs.X/main.c
@@ -0,0 +1,26 @@
+// (c) Muthanna A. Attyah Mar 2017
+// using PC16F18875 on Curiosity HPC
+
+#include "mcc_generated_files/mcc.h"
+
+
+void main(void)
+{
+
+    SYSTEM_Initialize();
+                 
+    IO_RA4_SetHigh();
+    IO_RA5_SetLow();
+    IO_RA6_SetLow();
+    IO_RA7_SetHigh();
+    
+    while (1){
+               
+        IO_RA4_Toggle();
+        IO_RA5_Toggle();
+        IO_RA6_Toggle();
+        IO_RA7_Toggle();
+        __delay_ms(500);
+    }
+
+}
