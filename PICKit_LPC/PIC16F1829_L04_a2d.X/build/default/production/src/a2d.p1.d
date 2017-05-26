993883fd4124beda961e6e66fcd57d2e6774fa80
mkhuthir <mkhuthir@gmail.com>
2017-05-26 10:56:02 +0400

    LPC

---
+++ PICKit_LPC/PIC16F1829_L04_a2d.X/build/default/production/src/a2d.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/src/a2d.d  \
+ build/default/production/src/a2d.p1:  \
+ src/a2d.c 
---
+++ PICKit_LPC/PIC16F1829_L13_eeprom.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PIC16F1829_L13_eeprom.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PIC16F1829_L13_eeprom.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pic16f1829l13eeprom.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pic16f1829l13eeprom.x.tar
