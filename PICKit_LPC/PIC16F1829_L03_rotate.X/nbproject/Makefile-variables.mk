1cfc363ca1c4cf6e3888845e73cac09583bde751
mkhuthir <mkhuthir@gmail.com>
2017-05-26 08:11:32 +0400

    LPC

---
+++ PICKit_LPC/PIC16F1829_L03_rotate.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PIC16F1829_L03_rotate.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PIC16F1829_L03_rotate.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pic16f1829l03rotate.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pic16f1829l03rotate.x.tar
