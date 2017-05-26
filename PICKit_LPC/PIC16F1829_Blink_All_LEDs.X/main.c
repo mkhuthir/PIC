b73e25d3f4be2b0480b75700dc08b274f9c7d245
mkhuthir <mkhuthir@gmail.com>
2017-05-26 20:04:56 +0400

    PICKit MCC

---
+++ PICKit_LPC/PIC16F1829_Blink_All_LEDs.X/main.c
@@ -0,0 +1,40 @@
+
+
+#include "mcc_generated_files/mcc.h"
+
+
+void main(void)
+{
+    // initialize the device
+    SYSTEM_Initialize();
+
+    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+    
+    LED_DS1_SetHigh();
+    LED_DS2_SetHigh();
+    LED_DS3_SetHigh();
+    LED_DS4_SetHigh();
+    
+    while (1)
+    {
+        LED_DS1_Toggle();
+        LED_DS2_Toggle();
+        LED_DS3_Toggle();
+        LED_DS4_Toggle();
+        
+        __delay_ms(1000);
+    }
+}
--- PICKit_LPC/PIC16F1829_L01_hello_world.X/src/hello_world.c
+++ PICKit_LPC/PIC16F1829_L01_hello_world.X/src/hello_world.c
@@ -23,11 +23,28 @@
  * ******************************************************************
  */
 
-#include <htc.h>                            //PIC hardware mapping
+#include <xc.h>                            //PIC hardware mapping
 
-//config bits that are part-specific for the PIC16F1829
-__CONFIG(FOSC_INTOSC & WDTE_OFF & PWRTE_OFF & MCLRE_OFF & CP_OFF & CPD_OFF & BOREN_ON & CLKOUTEN_OFF & IESO_OFF & FCMEN_OFF);
-__CONFIG(WRT_OFF & PLLEN_OFF & STVREN_OFF & LVP_OFF);
+// Config bits that are part-specific for the PIC16F1829
+// CONFIG1
+#pragma config FOSC = INTOSC        // Oscillator Selection->INTOSC oscillator: I/O function on CLKIN pin
+#pragma config WDTE = OFF            // Watchdog Timer Enable->WDT disabled
+#pragma config PWRTE = OFF          // Power-up Timer Enable->PWRT disabled
+#pragma config MCLRE = OFF          // MCLR Pin Function Select->MCLR/VPP pin function is MCLR
+#pragma config CP = OFF                // Flash Program Memory Code Protection->Program memory code protection is disabled
+#pragma config CPD = OFF              // Data Memory Code Protection->Data memory code protection is disabled
+#pragma config BOREN = ON           // Brown-out Reset Enable->Brown-out Reset enabled
+#pragma config CLKOUTEN = OFF    // Clock Out Enable->CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin
+#pragma config IESO = OFF             // Internal/External Switchover->Internal/External Switchover mode is enabled
+#pragma config FCMEN = OFF          // Fail-Safe Clock Monitor Enable->Fail-Safe Clock Monitor is enabled
+
+// CONFIG2
+#pragma config WRT = OFF            // Flash Memory Self-Write Protection->Write protection off
+#pragma config PLLEN = OFF          // PLL Enable->4x PLL disabled
+#pragma config STVREN = OFF       // Stack Overflow/Underflow Reset Enable->Stack Overflow or Underflow will cause a Reset
+#pragma config BORV = LO            // Brown-out Reset Voltage Selection->Brown-out Reset Voltage (Vbor), low trip point selected.
+#pragma config LVP = OFF            // Low-Voltage Programming Enable->Low-voltage programming disabled
+
 
     /* -------------------LATC-----------------
      * Bit#:  -7---6---5---4---3---2---1---0---
