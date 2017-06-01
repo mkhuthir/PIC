57016cd81c0c4b0a6eada12b366ad6507118e1f0
mkhuthir <mkhuthir@gmail.com>
2017-06-01 16:34:05 +0400

    fade

---
+++ Xpress_PIC16F18855/CIP_Fade.X/mcc_generated_files/pin_manager.c
@@ -0,0 +1,122 @@
+/**
+  Generated Pin Manager File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    pin_manager.c
+
+  Summary:
+    This is the Pin Manager file generated using MPLAB(c) Code Configurator
+
+  Description:
+    This header file provides implementations for pin APIs for all pins selected in the GUI.
+    Generation Information :
+        Product Revision  :  MPLAB(c) Code Configurator - 4.15.1
+        Device            :  PIC16F18855
+        Driver Version    :  1.02
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 1.35
+        MPLAB             :  MPLAB X 3.40
+
+    Copyright (c) 2013 - 2015 released Microchip Technology Inc.  All rights reserved.
+
+    Microchip licenses to you the right to use, modify, copy and distribute
+    Software only when embedded on a Microchip microcontroller or digital signal
+    controller that is integrated into your product or third party product
+    (pursuant to the sublicense terms in the accompanying license agreement).
+
+    You should refer to the license agreement accompanying this Software for
+    additional information regarding your rights and obligations.
+
+    SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+    EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+    MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+    IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+    CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+    OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+    INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+    CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+    SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+    (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+*/
+
+#include <xc.h>
+#include "pin_manager.h"
+#include "stdbool.h"
+
+
+void PIN_MANAGER_Initialize(void)
+{
+    /**
+    LATx registers
+    */   
+    LATA = 0x00;    
+    LATB = 0x00;    
+    LATC = 0x00;    
+
+    /**
+    TRISx registers
+    */    
+    TRISA = 0xF0;
+    TRISB = 0xFF;
+    TRISC = 0xFF;
+
+    /**
+    ANSELx registers
+    */   
+    ANSELC = 0xDF;
+    ANSELB = 0x7F;
+    ANSELA = 0xEF;
+
+    /**
+    WPUx registers
+    */ 
+    WPUE = 0x08;
+    WPUB = 0xFF;
+    WPUA = 0xFF;
+    WPUC = 0xFF;
+
+    /**
+    ODx registers
+    */   
+    ODCONA = 0x00;
+    ODCONB = 0x00;
+    ODCONC = 0x00;
+    
+
+
+   
+    
+    
+    bool state = GIE;
+    GIE = 0;
+    PPSLOCK = 0x55;
+    PPSLOCK = 0xAA;
+    PPSLOCKbits.PPSLOCKED = 0x00; // unlock PPS
+
+    T6AINPPSbits.T6AINPPS = 0x0F;   //RB7->TMR6:T6IN;
+    T4AINPPSbits.T4AINPPS = 0x15;   //RC5->TMR4:T4IN;
+    CLCIN0PPSbits.CLCIN0PPS = 0x04;   //RA4->CLC1:CLCIN0;
+    RA3PPS = 0x01;   //RA3->CLC1:CLC1OUT;
+    RA1PPS = 0x01;   //RA1->CLC1:CLC1OUT;
+    RA2PPS = 0x01;   //RA2->CLC1:CLC1OUT;
+    RA0PPS = 0x01;   //RA0->CLC1:CLC1OUT;
+
+    PPSLOCK = 0x55;
+    PPSLOCK = 0xAA;
+    PPSLOCKbits.PPSLOCKED = 0x01; // lock PPS
+
+    GIE = state;
+}       
+
+void PIN_MANAGER_IOC(void)
+{   
+
+}
+
+/**
+ End of File
+*/
