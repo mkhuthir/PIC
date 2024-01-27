3ec0a566c867d2202c88aae5b26a94afd35b22a6
Muthanna Alwahash <mkhuthir@gmail.com>
2024-01-27 22:23:12 +0400

    w

---
+++ PIC16F18875/I2C_BlinkM.X/BlinkM/BlinkM.c
@@ -0,0 +1,340 @@
+// BlinkM Library
+// Muthanna A. Attyah (June 2017)
+// Please feel free to copy and use code.
+// Device data sheet https://thingm.com/fileadmin/thingm/downloads/BlinkM_datasheet.pdf
+
+/** How the BlinkM is connected to Curiosity HPC Board:
+ 
+    BlankM  MikroBus    Curiosity HPC   Remarks
+-------------------------------------------------------------------
+    I2C C   SCL         RC3             I2C clock
+    I2C D   SDA         RC4             I2C data
+    PWR-    GND         GND             Ground
+    PWR+    +5V         +5V             Power supply
+-------------------------------------------------------------------
+ 
+ * RC3 & RC4 must have pull-up resistors enabled since BlinkM does not have pull-up resistors on its module.
+ 
+**/
+#include "../mcc_generated_files/system/system.h"
+#include "BlinkM.h"
+
+
+
+//---------------------------------------------------------------------------------------------
+// Writes nD number of bytes to I2C device with I2C address=Slave_Adr using Microchip MSSP I2C1 driver.
+
+bool I2C_Write(char *pData, char nD)
+{    
+    static I2C1_TRANSACTION_REQUEST_BLOCK trb;              // TRB
+    I2C1_MESSAGE_STATUS status = I2C1_MESSAGE_PENDING;      // initial status is pending
+    uint8_t timeOut=0;                                      // used to count retries
+
+    I2C1_MasterWriteTRBBuild(&trb, pData, nD, Slave_Adr);   // build a Write TRB
+
+    while(status != I2C1_MESSAGE_FAIL)                      // if status is  I2C1_MESSAGE_ADDRESS_NO_ACK, or I2C1_DATA_NO_ACK,
+    {                                                       // The device may be busy and needs more time for the last write.
+        I2C1_MasterTRBInsert(1, &trb, &status);             // send the full TRB parameters > (length, data, status)
+        while(status == I2C1_MESSAGE_PENDING);              // wait for the message to be sent or status has changed.
+        if (status == I2C1_MESSAGE_COMPLETE) break;         // exit loop if status is complete
+        if (timeOut == 30) break;                           // check if max retry=30 is reached
+        else timeOut++;                                     // try again
+    }
+    return (status == I2C1_MESSAGE_COMPLETE);               // return the status
+}
+
+//---------------------------------------------------------------------------------------------
+// Sends nC bytes command and Reads nD number of bytes from I2C device with
+// I2C address=Slave_Adr using Microchip MSSP I2C1 driver.
+
+bool I2C_Read(char *pCmnd, char nC, char*pData, char nD)
+{    
+    
+    static I2C1_TRANSACTION_REQUEST_BLOCK trb[2];           // TRB
+    I2C1_MESSAGE_STATUS status = I2C1_MESSAGE_PENDING;      // initial status is pending
+    uint8_t timeOut=0;                                      // used to count retries
+
+    I2C1_MasterWriteTRBBuild(&trb[0], pCmnd, nC, Slave_Adr);// build a Write TRB
+    I2C1_MasterReadTRBBuild (&trb[1], pData, nD, Slave_Adr);// build a Read TRB
+    
+    while(status != I2C1_MESSAGE_FAIL)                      // if status is  I2C1_MESSAGE_ADDRESS_NO_ACK, or I2C1_DATA_NO_ACK,
+    {                                                       // The device may be busy and needs more time for the last write.
+        I2C1_MasterTRBInsert(2, &trb[0], &status);          // send the full TRB parameters > (length, data, status)
+        while(status == I2C1_MESSAGE_PENDING);              // wait for the message to be sent or status has changed.
+        if (status == I2C1_MESSAGE_COMPLETE) break;         // exit loop if status is complete
+        if (timeOut == 30) break;                           // check if max retry=30 is reached
+        else timeOut++;                                     // try again
+    }
+    return (status == I2C1_MESSAGE_COMPLETE);               // return the status
+}
+
+//---------------------------------------------------------------------------------------------
+// Sets the BlinkM to a particular RGB color immediately.The command takes
+// three argument bytes, one each for setting the levels of the red, green, and blue channels.
+// Each value ranges from 0-255 (0x00-0xFF in hexadecimal), with 0 being off and 255 being
+// maximum brightness.
+
+void GoToRGB(char R, char G, char B)
+{
+    char data[4]={'n',0,0,0};
+    data[1]=R;
+    data[2]=G;
+    data[3]=B;
+    I2C_Write(data,4);
+};
+
+//---------------------------------------------------------------------------------------------
+// Fades from the current color to the specified RGB color.The command takes three argument
+// bytes, one each for setting the levels of the red, green, and blue channels. Each value 
+// ranges from 0-255 (0x00-0xFF in hexadecimal), with 0 being off and 255 being maximum brightness.
+// The rate at which the fading occurs is controlled by the SetFadeSpeed() command. 
+
+void FadeToRGB(char R, char G, char B)
+{
+    char data[4]={'c',0,0,0};
+    data[1]=R;
+    data[2]=G;
+    data[3]=B;
+    I2C_Write(data,4);
+}
+
+//---------------------------------------------------------------------------------------------
+// Fades from the current color to the specified HSB color.The command takes
+// three bytes as arguments. The first argument byte is the hue (or raw color), with mapping from 0-255.
+// The second argument is the saturation, or vividness, of the color. A saturation of 0 means a
+// very light/white color and a saturation of 255 means a very vivid color. The third argument is
+// the brightness of the resulting color, where 0 is totally dark and 255 means maximally bright. 
+// The rate at which the fading occurs is controlled by the SetFadeSpeed() command. 
+
+void FadeToHSB(char H, char S, char B)
+{
+    char data[4]={'h',0,0,0};
+    data[1]=H;
+    data[2]=S;
+    data[3]=B;
+    I2C_Write(data,4);
+}
+
+//---------------------------------------------------------------------------------------------
+// Fades from the current color to a random color. It takes 3 bytes as arguments,
+// one for each R,G,B channel. Each argument is the range or amount of randomness for each
+// of the R,G,B channels from which to deviate from the current color.
+// A setting of 0 for a channel means to not change it at all.
+
+void FadeToRndRGB(char R, char G, char B)
+{
+    char data[4]={'C',0,0,0};
+    data[1]=R;
+    data[2]=G;
+    data[3]=B;
+    I2C_Write(data,4);
+}
+
+//---------------------------------------------------------------------------------------------
+// Fades from the current color to a random color. It takes 3 bytes as arguments,
+// one for each H,S, B value. Each argument is the range or degree of randomness to deviate
+// from the current H,S,B color. A setting of 0 for a channel means to not change it at all.
+// *Note* that this command only works after a previous FadeToHSB() command has been used to set an
+// initial hue.
+
+void FadeToRndHSB(char H, char S, char B)
+{
+    char data[4]={'H',0,0,0};
+    data[1]=H;
+    data[2]=S;
+    data[3]=B;
+    I2C_Write(data,4);
+}
+
+//---------------------------------------------------------------------------------------------
+// Plays the specified light script immediately, stopping any currently playing script. 
+// The command takes three bytes as arguments. The first byte is the script id of the script
+// to play. The second argument is the number of repeats to play the script.
+// A repeats value of 0 means play the script forever. The last argument is the script line
+// number to start playing from. A value of 0 means play the script from the start.
+// To adjust the playback speed of a script that is running, adjust the fade speed (SetFadeSpeed()
+// and time adjust (SetTimeAdjust() to taste. Altering these values can greatly alter the lighting
+// effect for the built-in light scripts. See data sheet for list of scripts IDs.
+
+void PlayLightScript(char n, char r, char p)
+{
+    char data[4]={'p',0,0,0};
+    data[1]=n;
+    data[2]=r;
+    data[3]=p;
+    I2C_Write(data,4);
+}
+
+//---------------------------------------------------------------------------------------------
+// Stops any currently playing script. If no script is playing, this command has no
+// effect. It takes no arguments and returns no value.
+
+void StopScript()
+{
+    char data[1]={'o'};
+    I2C_Write(data,1);
+}
+
+//---------------------------------------------------------------------------------------------
+// Sets the rate at which color fading happens. It takes one argument that is the
+// fade speed from 1-255. The slowest fading occurs when the fade speed is 1. To change
+// colors instantly, set the fade speed to 255. A value of 0 is invalid and is reserved for a future
+// Smart Fade feature.
+
+void SetFadeSpeed(char f)
+{
+    char data[2]={'f',0};
+    data[1]=f;
+    I2C_Write(data,2);
+}
+
+//---------------------------------------------------------------------------------------------
+// Adjusts the playback speed of a light script. It takes one byte as an argument,
+// a signed number between -128 and 127. The argument is treated as an additive adjustment to
+// all durations of the script being played. A value of 0 resets the playback speed to the default.
+// This command does not return a value.
+
+void SetTimeAdjust(char t)
+{
+    char data[2]={'t',0};
+    data[1]=t;
+    I2C_Write(data,2);
+}
+
+//---------------------------------------------------------------------------------------------
+// Returns the current color in RGB format. The command takes no argument bytes but returns
+// 3 bytes representing the current values of the red, green and blue channels.
+// *Note* If the BlinkM is currently fading between colors, this command returns the instantaneous
+// current color value, not the destination color.
+
+void GetCurrentRGB(char *data)
+{
+    char cmnd[1]={'g'};
+    I2C_Read(cmnd,1,data,3);
+}
+
+//---------------------------------------------------------------------------------------------
+// This command writes a light script line. The first argument is which script id to write to.
+// Currently, only script id 0 can be written to. The second argument is which line in the script to
+// change, and can range from 0-49. The third argument is the duration in ticks for that
+// command to last. The next four arguments are the BlinkM command and its arguments. Any
+// command with less than 3 arguments should fill out the remaining arguments slots with zeros.
+// This command takes approximately 20 milliseconds to complete, due to EEPROM write time.
+// Once all the lines of the desired script are written, set the script length with the SetScriptLength
+// command. This command does not return a value.
+
+void WriteScriptLine(char n, char p, char d, char c, char a1, char a2, char a3)
+{
+    char data[8]={'W',0,0,0,0,0,0,0};
+    data[1]=n;
+    data[2]=p;
+    data[3]=d;
+    data[4]=c;
+    data[5]=a1;
+    data[6]=a2;
+    data[7]=a3;
+    I2C_Write(data,8);
+    __delay_ms(20);         //wait for EEPROM to finish write
+}
+
+//---------------------------------------------------------------------------------------------
+// Reads a script line and returns the script lines values. The first argument is the
+// script id to read from. Script id 0 is the EEPROM script that can be written to, Script ids >0 refer
+// to the built-in ROM scripts. The second argument is the number of the script line to read back.
+// There are 5 bytes of return values: d = duration in ticks, c = command, a1,2,3 = arguments for
+// command. If an invalid script id or script line number is given, all return values are zeros.
+        
+void ReadScriptLine(char n, char p, char* data)
+{
+    char cmnd[3]={'R',0,0};
+    cmnd[1]=n;
+    cmnd[2]=p;
+    I2C_Read(cmnd,3,data,5);
+}
+
+//---------------------------------------------------------------------------------------------
+// Sets the length of a written script. The first argument is the script id to set,
+// currently only script id of 0 is valid. The second argument is the length of the script, and the
+// third argument is the number of repeats for the script. This command takes approximately 15
+// milliseconds to complete, due to EEPROM write times.
+// This command does not return a value.
+    
+void SetScriptLength(char n, char l, char r)
+{
+    char data[4]={'L',0,0,0};
+    data[1]=n;
+    data[1]=l;
+    data[1]=r;
+    I2C_Write(data,4);
+    __delay_ms(15);         //wait for EEPROM to finish write
+
+}
+
+//---------------------------------------------------------------------------------------------
+// Sets the I2C address of a BlinkM. It takes four arguments. The first and last
+// argument are the new address, and the second and third arguments are {0xd0,0x0d}. These
+// two arguments are used as a check against inadvertent address changing. This command can
+// be used with the I2C general call broadcast address to change the address of a BlinkM if the
+// previous address is not known. When using general call, only have one BlinkM powered up on
+// the bus at a time or they will all change their address. This command takes approximately 15
+// milliseconds to complete, due to EEPROM write time and I2C stack reset.
+// See data sheet for more details about how BlinkM handles I2C addresses.
+// This command does not return a value.
+        
+void SetBlinkMAdr(char a)
+{
+    char data[5]={'A',0,0xD0,0x0D,0};
+    data[1]=a;
+    data[4]=a;
+    I2C_Write(data,5);
+    __delay_ms(15);         //wait for EEPROM to finish write
+
+}
+
+//---------------------------------------------------------------------------------------------
+// Returns the I2C address.
+
+void GetBlinkMAdr(char* data)
+{
+    char cmnd[1]={'a'};
+    I2C_Read(cmnd,1,data,1);
+}
+
+//---------------------------------------------------------------------------------------------
+// Returns the BlinkM firmware version. The first byte is the major version, the second byte is the
+// minor version.
+
+void GetBlinkMVer(char* data)
+{
+    char cmnd[1]={'Z'};
+    I2C_Read(cmnd,1,data,2);
+    
+}
+
+//---------------------------------------------------------------------------------------------
+// Sets the startup (or boot) action for BlinkM. The command takes four
+// arguments. The first argument m is the startup mode: 0 means do nothing, 1 means play a
+// script. The second argument n is which script id to play. The third argument f is the number
+// of repetitions to play that script id. The fourth (f) and fifth (t) arguments are the fade speed
+// and time adjust, respectively, to use with the script. This command takes about 20
+// milliseconds to complete, due to EEPROM write time.
+// Note: when turning off playing a script by setting the first argument m to 0, the other
+// arguments are saved but not loaded on startup and instead set to zero. This is most
+// noticeable with the fade speed value. Thus if a {B,0,...} is issued to disable startup script
+// playing, be sure to issue a {f, 20} command after BlinkM startup or color fading will not
+// work.
+// This command does not return a value.
+        
+void SetStartup(char m, char n, char r, char f, char t)
+{
+    char data[6]={'B',0,0,0,0,0};
+    data[1]=m;
+    data[2]=n;
+    data[3]=r;
+    data[4]=f;
+    data[5]=t;
+    I2C_Write(data,6);
+    __delay_ms(20);         //wait for EEPROM to finish write
+}
+
+//---------------------------------------------------------------------------------------------
--- PIC16F18875/I2C_BlinkM.X/main.c
+++ PIC16F18875/I2C_BlinkM.X/main.c
@@ -1,14 +1,216 @@
-// Muthanna Alwahash
-// Jan 2024
+// BlinkM Library
+// Muthanna A. Attyah (June 2017-2024)
+// Please feel free to copy and use code.
+// Device data sheet https://thingm.com/fileadmin/thingm/downloads/BlinkM_datasheet.pdf
+
+/** How the BlinkM is connected to Curiosity HPC Board:
+ 
+    BlankM  MikroBus    Curiosity HPC   Remarks
+-------------------------------------------------------------------
+    I2C C   SCL         RC3             I2C clock
+    I2C D   SDA         RC4             I2C data
+    PWR-    GND         GND             Ground
+    PWR+    +5V         +5V             Power supply
+-------------------------------------------------------------------
+ 
+ * RC3 & RC4 must have pull-up resistors enabled since BlinkM does not have pull-up resistors on its module.
+ 
+**/
+
 
 #include "mcc_generated_files/system/system.h"
+#include "BlinkM.h"
 
-int main(void)
+void WaitKeyPress()
 {
+    while(S1_GetValue());         // Wait until S1 key is pressed
+    __delay_ms(100);
+    while(!S1_GetValue());        // Wait until S1 key is released
+    __delay_ms(100);
+}
+
+// Main code
+void main(void)
+{
+    // Setup and initializations
     SYSTEM_Initialize();
+    INTERRUPT_GlobalInterruptEnable();
+    INTERRUPT_PeripheralInterruptEnable();
+    
+   
+    // Testing all functions..
+    // Demo Execution loop
+    while(1){
+        
+        printf("\x0C");                             // Terminal Form Feed to erase screen
+        printf("\n\nWelcome to BlinkM Demo..\n");
 
-    while(1)
-    {
+        // Print current BlinkM I2C Address, Firmware Version, and one selected script line from EEPROM.
+        
+        char Adr[1]={0};        // to read I2C address
+        char Ver[2]={0,0};      // to read firmware version major and minor numbers
+        char RGB[3]={0,0,0};    // to read the current RGB values
+        char SL[5]={0,0,0,0,0}; // to read one script line
+        
+        GetBlinkMAdr(&Adr[0]);
+        GetBlinkMVer(&Ver[0]);
+        
+        printf("BlinkM info:\n\n");
+        printf("I2C Address\t:0x%02X\n",Adr[0]);
+        printf("Firmware Ver.\t:0x%02X - 0x%02X\n\n",Ver[0],Ver[1]);
+        
+        printf("Reading Script Line #0 from EEPROM Script #0:\n");
+        ReadScriptLine(0,0,&SL[0]);
+        printf("Line 0: Duration=0x%02X, Command=0x%02X, R=0x%02X, G=0x%02X, B=0x%02X\n\n",SL[0],SL[1],SL[2],SL[3],SL[4]);
 
-    }    
+        printf("Setting the startup to play the green_flash script..\n");
+        SetStartup(1,4,5,255,-15);          // Set the Startup to play 4th script
+        
+        printf("Turn LED off and stop script...\n\n");
+        GoToRGB(0x00, 0x00, 0x00);
+        StopScript();
+        
+        printf("Ready to start the Demo...press S1 to start.\n");
+        LED_D2_SetHigh();               // All Dx LEDs On.
+        LED_D3_SetHigh();
+        LED_D4_SetHigh();
+        LED_D5_SetHigh();
+        WaitKeyPress();
+        
+        // White
+        printf("White");
+        GoToRGB(0xFF, 0xFF, 0xFF);      // Turn LED On
+                
+        GetCurrentRGB(&RGB[0]);
+        printf("\tRGB value (0x%02X,0x%02X,0x%02X)...press S1.\n",RGB[0],RGB[1],RGB[2]);
+        
+        WaitKeyPress();
+        
+        // Red
+        printf("Red");
+        GoToRGB(0xFF, 0x00, 0x00);
+        GetCurrentRGB(&RGB[0]);
+        printf("\tRGB value (0x%02X,0x%02X,0x%02X)...press S1.\n",RGB[0],RGB[1],RGB[2]);
+        WaitKeyPress();
+        
+        // Green
+        printf("Green");
+        GoToRGB(0x00, 0xFF, 0x00);  
+        GetCurrentRGB(&RGB[0]);
+        printf("\tRGB value (0x%02X,0x%02X,0x%02X)...press S1.\n",RGB[0],RGB[1],RGB[2]);
+        WaitKeyPress();
+        
+        // Blue
+        printf("Blue");
+        GoToRGB(0x00, 0x00, 0xFF);
+        GetCurrentRGB(&RGB[0]);
+        printf("\tRGB value (0x%02X,0x%02X,0x%02X)...press S1.\n",RGB[0],RGB[1],RGB[2]);
+        WaitKeyPress();
+        
+        // Yellow
+        printf("Yellow");
+        GoToRGB(0xFF, 0xFF, 0x00);
+        GetCurrentRGB(&RGB[0]);
+        printf("\tRGB value (0x%02X,0x%02X,0x%02X)...press S1.\n",RGB[0],RGB[1],RGB[2]);
+        WaitKeyPress();
+        
+        // Cyan
+        printf("Cyan");
+        GoToRGB(0x00, 0xFF, 0xFF);
+        GetCurrentRGB(&RGB[0]);
+        printf("\tRGB value (0x%02X,0x%02X,0x%02X)...press S1.\n",RGB[0],RGB[1],RGB[2]);
+        WaitKeyPress();
+        
+        // Magenta
+        printf("Magenta");
+        GoToRGB(0xFF, 0x00, 0xFF);
+        GetCurrentRGB(&RGB[0]);
+        printf("\tRGB value (0x%02X,0x%02X,0x%02X)...press S1.\n",RGB[0],RGB[1],RGB[2]);
+        WaitKeyPress();
+        
+        // Slow Fade
+        printf("Setting Slow Fade\n");
+        SetFadeSpeed(4);
+        
+        // Fade to Red 
+        printf("Fade to Red...press S1.\n");
+        FadeToRGB(0xFF, 0x00, 0x00);
+        WaitKeyPress();
+        
+        // Fade to Random RGB
+        printf("Fade to random RGB...press S1.\n");
+        FadeToRndRGB(0xFF, 0xFF, 0xFF);
+        WaitKeyPress();
+        
+        // Fade to 0x60 HSB
+        printf("Fade to 0x60 HSB...press S1.\n");
+        FadeToHSB(0x60, 0xFF, 0xFF);
+        WaitKeyPress();
+        
+        // Fade to Random HSB
+        printf("Fade to random HSB\n");
+        FadeToRndHSB(0xFF, 0xFF, 0xFF);
+        WaitKeyPress();
+        
+        // No fade and faster time
+        printf("Setting no fade and faster time\n");
+        SetFadeSpeed(255);
+        SetTimeAdjust(-2);
+        
+        // Play script 'RGB' 2 times start from first line default time speed
+        printf("Play RGB script...press S1.\n");
+        PlayLightScript(RGB, 2, 0);
+        WaitKeyPress();
+        
+        // Default Fade and Fast time
+        printf("Setting fast time\n");
+        SetTimeAdjust(-2);
+
+        // Play script 'blue_flash' 5 times start from first line fast time speed
+        printf("Play blue_flash script...press S1.\n");
+        PlayLightScript(blue_flash, 5, 0);
+        WaitKeyPress();
+        
+        // Put back the default speed.
+        printf("Setting default fade and time speed\n");
+        SetFadeSpeed(0);
+        SetTimeAdjust(0);
+        
+        // Play script 'virtual_candle' 2 times start from first line default fade speed
+        printf("Play virtual_candle...press S1.\n");
+        PlayLightScript(virtual_candle, 2, 0);
+        WaitKeyPress();
+        
+        // Play script 'morse_code_SOS' 2 times start from first line
+        printf("Play morse_code script ...press S1.\n");
+        PlayLightScript(morse_code_SOS, 2, 0);
+        WaitKeyPress();
+        
+        // Build a new script
+        printf("Building a new script\n");
+        SetScriptLength(0,7,3);                         // Script 0 is 7 lines, repeat it 3 times
+        WriteScriptLine(0,0,15,'n',0xFF,0x00,0x00);     // Line 1
+        WriteScriptLine(0,1,15,'n',0x00,0xFF,0x00);     // Line 2
+        WriteScriptLine(0,2,15,'n',0x00,0x00,0xFF);     // Line 3
+        WriteScriptLine(0,3,15,'n',0xFF,0xFF,0x00);     // Line 4
+        WriteScriptLine(0,4,15,'n',0xFF,0x00,0xFF);     // Line 5
+        WriteScriptLine(0,5,15,'n',0x00,0xFF,0xFF);     // Line 6
+        WriteScriptLine(0,6,15,'n',0xFF,0xFF,0xFF);     // Line 7
+        
+        // Play the new script one time
+        printf("Play the new script...press S1.\n");
+        PlayLightScript(0, 1, 0);
+        WaitKeyPress();
+        
+        //Demo is done...Blink all LEDs and wait for a key press to start another demo cycle.
+        printf("Play hue_cycle Script...\n");
+        printf("\n\nDemo is Done...Thanks for watching\npress S1 to start another one!\n");
+        PlayLightScript(hue_cycle,0,0);
+        LED_D2_SetLow();                                // All Dx LEDs Off.
+        LED_D3_SetLow();
+        LED_D4_SetLow();
+        LED_D5_SetLow();
+        WaitKeyPress();
+    }
+        
 }
