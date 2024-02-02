bfae1b15dd90fd073e82c837c8c66e138926f70d
Muthanna Alwahash <mkhuthir@gmail.com>
2024-02-02 22:17:07 +0400

    w

--- PIC16F18875/I2C_Scan.X/main.c
+++ PIC16F18875/I2C_Scan.X/main.c
@@ -34,6 +34,22 @@ void main(void)
         D3_SetHigh();
         D4_SetHigh();
         D5_SetHigh();
+
+        uint8_t i2c_addr;
+        uint8_t i2c_data[2];  
+
+        for (i2c_addr=1; i2c_addr<128; i2c_addr++)
+        {
+            printf("Checking %d\n\r", i2c_addr);
+            I2C1_Read(i2c_addr, i2c_data, 1);
+            while(I2C1_IsBusy());
+            __delay_ms(100);
+            if (I2C_ERROR_NONE == I2C1_ErrorGet())
+            {
+                printf("device found at address %d\n\r",i2c_addr);
+            }
+
+        }
         
         printf("Press S1 to end\n\r");
         WaitKeyPress();
